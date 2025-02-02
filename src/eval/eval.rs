5ee37fc72ce09b685dd96968290ec95789400d9f
Boburmirzo <boburforfun@gmail.com>
2025-02-02 21:03:19 +0500

    feat: evaluating infix

--- src/eval/eval.rs
+++ src/eval/eval.rs
@@ -1,6 +1,46 @@
-use crate::objects::objects::Object;
+use crate::ast::ast::{self, Expression};
+use crate::objects::objects;
 
+pub fn eval_program(program:Vec<ast::Statement>) -> objects::Object {
+    let mut result: objects::Object = objects::Object::Null;
+    for s in program{
+        result = eval_stmnt(s); 
+    }
+    result
+}
 
-pub fn eval() -> Option<Object>{
-    Some(Object::Null)
+fn eval_stmnt(stmnt:ast::Statement) -> objects::Object {
+    match stmnt {
+        ast::Statement::ExprsStatement { token: _, exprs } => eval_exprs(exprs),
+        _ => objects::Object::Null
+    }
+}
+
+fn eval_exprs(s:ast::Expression) -> objects::Object  {
+    match s {
+        ast::Expression::Int(s) => objects::Object::Int(s),
+        ast::Expression::Boolean{token: _, value} => objects::Object::Bool(value),
+        ast::Expression::InfixExprsn { left, right, oprt } => eval_infix(*left, *right, oprt),
+        _ =>  objects::Object::Null
+    }
+}
+
+fn eval_infix(left:ast::Expression, right:Expression, oprtr:String) -> objects::Object {
+        let right = eval_exprs(right);
+        let left = eval_exprs(left);
+        match (right, left) {
+            (objects::Object::Int(a), objects::Object::Int(b)) => {
+                match oprtr.as_str(){
+                    "+" => objects::Object::Int(a + b),
+                    "-" => objects::Object::Int(a - b),
+                    "*" => objects::Object::Int(a * b),
+                    "/" => objects::Object::Int(a / b),
+                    _ => objects::Object::Null
+                }
+            },
+            _ => {
+                println!("oops mismatched types");
+                objects::Object::Null
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
+use eval::eval::eval_program;
+
+mod eval;
 mod lexer;
 mod objects;
-mod eval;
 mod token;
 mod ast;
 mod parser;
 fn main() {
     let src = String::from("
-        fn (param, param2){
-            return 1 + 1;
-        }
+        ((1 + 2) * 2) / 6;
         ");
     let lexer = lexer::Lexer::new(src);
     let mut parser = parser::Parser::new(Box::new(lexer));
     let statmnts = parser.parse_program();
-    println!("node: {:?}", statmnts);
-    println!("number of statements: {}", statmnts.len())
+    let result = eval_program(statmnts);
+    println!("{}", result)
 }
